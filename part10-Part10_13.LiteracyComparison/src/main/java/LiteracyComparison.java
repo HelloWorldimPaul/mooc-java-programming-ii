d0e2a75b759954708b042ddf4131b293c82e4c5d
HelloPaul <Paulpedrano@gmail.com>
2025-08-24 19:13:54 +0800

    New activities done

--- part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -2,11 +2,39 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.ClientInfoStatus;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LiteracyComparison {
-    
+
     public static void main(String[] args) {
+
+
+        String fileName = "literacy.csv";
+        sortByLiteracy(fileName);
+
+    }
+
+    public static void sortByLiteracy(String fileName){
+
+
+        try (Stream<String> lines = Files.lines(Paths.get(fileName))) {
+
+            lines.map(row -> row.split(","))
+                    .filter(parts -> parts.length == 6)
+
+                    .map(parts -> new LiteracyRate(parts[0], parts[1], parts[2] = parts[2].replaceAll("[^a-zA-Z\\s]", "").trim() , parts[3], Integer.parseInt(parts[4]), Double.parseDouble(parts[5])))
+                    .sorted((p1, p2) -> Double.compare(p1.getLiteracyPercent(), p2.getLiteracyPercent()))
+                    .forEach(rate -> System.out.println(rate) );
+
+
+        } catch (Exception e) {
+
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 }
---
+++ part10-Part10_13.LiteracyComparison/src/main/java/LiteracyRate.java
@@ -0,0 +1,36 @@
+import javax.annotation.processing.SupportedOptions;
+
+public class LiteracyRate {
+
+    private String theme;
+    private String ageGroup;
+    private String gender;
+    private String country;
+    private int year;
+    private double literacyPercent;
+
+
+    public LiteracyRate(String theme, String ageGroup, String gender, String country, int year, double literacyPercent) {
+
+        this.theme = theme;
+        this.ageGroup = ageGroup;
+        this.gender = gender;
+        this.country = country;
+        this.year = year;
+        this.literacyPercent = literacyPercent;
+
+    }
+
+    public double getLiteracyPercent() {
+
+        return this.literacyPercent;
+
+    }
+
+    @Override
+    public String toString() {
+
+        return this.country + " (" + this.year + "), " + this.gender + ", " + this.literacyPercent;
+
+    }
+}
