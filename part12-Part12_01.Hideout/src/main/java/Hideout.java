47ef7f18904480e6769b2582ae6427ac227a768b
HelloPaul <Paulpedrano@gmail.com>
2025-08-30 20:11:19 +0800

    New Activites added

--- part12-Part12_01.Hideout/src/main/java/Hideout.java
+++ part12-Part12_01.Hideout/src/main/java/Hideout.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
 
-public class Hideout {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hideout<T> {
+
+   private T hidden;
+
+    public Hideout(){
+
+        this.hidden = null;
+    }
+
+
+
+    public void putIntoHideout(T toHide){
+
+
+        this.hidden = toHide;
+
+    }
+
+    public T takeFromHideout(){
+
+        T toTake = this.hidden;
+
+        this.hidden = null;
+
+        return toTake;
+
+
+    }
+
+    public boolean isInHideout(){
+
+       return this.hidden != null;
+
+    }
+
 }
--- part12-Part12_02.Pipe/src/main/java/Pipe.java
+++ part12-Part12_02.Pipe/src/main/java/Pipe.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Pipe {
+public class Pipe <T>{
+
+    List<T> pipe;
+
+    public Pipe(){
+
+        this.pipe = new ArrayList<>();
+    }
+
+
+
+
+    public void putIntoPipe(T value){
+
+
+        this.pipe.add(value);
+    }
+
+    public T takeFromPipe(){
+
+        if(this.pipe.isEmpty()){
+            return null;
+        }
+
+        T oldvalue = this.pipe.get(0);
+
+        this.pipe.remove(0);
+
+
+        return oldvalue;
+
+    }
+
+    public boolean isInPipe(){
+
+
+        return !this.pipe.isEmpty();
+    }
+
+
 }
