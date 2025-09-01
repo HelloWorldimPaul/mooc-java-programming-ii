a17e8e4d954c98cdb7dd22efa6f079b8037dff75
HelloPaul <Paulpedrano@gmail.com>
2025-09-01 13:15:19 +0800

    New Activites Done

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -1,11 +1,24 @@
 
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("How many random numbers should be printed?");
+        int userInput = scanner.nextInt();
+
+
+        for (int i = 0; i < userInput; i++) {
+
+            System.out.println(random.nextInt(11));
+
+        }
     }
 
 }
