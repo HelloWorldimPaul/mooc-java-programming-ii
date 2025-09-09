3b60dfa6227bb2bb4e986e5c98d356aa2fd69510
HelloPaul <Paulpedrano@gmail.com>
2025-09-09 18:06:37 +0800

    turned some codes that are having conflict for submission

--- part14-Part14_08.Hurray/src/main/java/hurraa/HurraaSovellus.java
+++ part14-Part14_08.Hurray/src/main/java/hurraa/HurraaSovellus.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.media.AudioClip;
+//import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -18,21 +18,21 @@ public class HurraaSovellus extends Application {
         Button nappi = new Button("Hurraa!");
         pane.setCenter(nappi);
 
-        AudioClip sound = new AudioClip(new File("PSYGANGNAM_STYLE.mp3").toURI().toString());
+        //AudioClip sound = new AudioClip(new File("PSYGANGNAM_STYLE.mp3").toURI().toString());
 
 
         nappi.setOnAction(e -> {
 
-            sound.play();
+            //sound.play();
 
         });
 
 
-        System.out.println(new File("Applause-Yannick_Lemieux.wav").getAbsolutePath());
-        System.out.println(new File("Applause-Yannick_Lemieux.wav").exists());
+        //System.out.println(new File("Applause-Yannick_Lemieux.wav").getAbsolutePath());
+        //System.out.println(new File("Applause-Yannick_Lemieux.wav").exists());
 
 
-        System.out.println(sound.getSource());
+        //System.out.println(sound.getSource());
 
 
         Scene scene = new Scene(pane, 600, 400);
